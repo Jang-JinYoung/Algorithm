4095a178f370e044ad1cb698b57929f7152bb18d
Jang-JinYoung <jinyoung_8965@naver.com>
2021-08-07 18:40:40 +0900

    sort
    sort

--- C/sort.c
+++ C/sort.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 #define SWAP(x,y,t) ( (t) = (x), (x) = (y), (y)=(t))
+
 void selection_sort(int nums[], int n);
+void insertion_sort(int nums[], int n);
+void bubble_sort(int nums[], int n);
 
 void main()
 {
@@ -14,10 +17,16 @@ void main()
 		nums[i] = rand() % n;	// 난수 발생 범위 0~n
 	}
 
-	selection_sort(nums, n);
+	//selection_sort(nums, n);
+
+	//insertion_sort(nums, n);
+
+	bubble_sort(nums, n);
+
+
 }
 
-//selection sort -> O(n²)
+//선택 정렬 -> O(n²)
 void selection_sort(int nums[], int n)
 {
 
@@ -32,4 +41,36 @@ void selection_sort(int nums[], int n)
 		}
 	}
 
+}
+
+//삽입 정렬 -> 최선 O(n) 최악 O(n²)
+void insertion_sort(int nums[], int n)
+{
+	int i, j, key;
+
+	for (i = 1; i < n; i++)
+	{
+		key = nums[i];
+		for (j = i - 1; j >= 0 && nums[j] > key; j--)
+		{
+			nums[j + 1] = nums[j];
+		}
+		nums[j + 1] = key;
+	}
+}
+
+//버블 정렬 -> O(n²)
+void bubble_sort(int nums[], int n)
+{
+	int i, j, temp;
+
+	for (int i = n - 1; i > 0; i--)
+	{
+		for (int j = 0; j < 1; j++) {
+			if (nums[j] > nums[j + 1])
+			{
+				SWAP(nums[j], nums[j + 1], temp);
+			}
+		}
+	}
 }
